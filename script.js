948f790eae258ae0ee6242e8cd1b33a2e4dee159
Himanshu Singh <himanshusingh1k@gmail.com>
2025-11-30 00:13:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 const codes = document.querySelectorAll(".code");
 codes[0].focus();
+
 codes.forEach((code, index) => {
-  code.addEventListener("input", (e) => {
-    const value = e.target.value;
-    if (value.length > 1) e.target.value = value.slice(0, 1);
-    if (value.length === 1 && index < codes.length - 1)
-      codes[index + 1].focus();
+  code.required = "true";
+
+  code.addEventListener("input", (evt) => {
+    const value = evt.target.value;
+    code.value = value;
+    if (index < codes.length - 1) codes[index + 1].focus();
   });
-  code.addEventListener("keydown", (e) => {
-    if (e.key === "Backspace") {
-      if (e.target.value === "" && index > 0) {
-        e.preventDefault();
-        codes[index - 1].focus();
-      } else e.target.value = "";
+
+  code.addEventListener("keydown", (evt) => {
+    if (evt.key === "Backspace") {
+      evt.preventDefault();
+      evt.target.value = "";
+      if (index > 0) codes[index - 1].focus();
     }
   });
 });
